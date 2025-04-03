58dcbdc4c37b70be90a8b7f558de79d864885137
lazar.stojcevic <stojcevic00@gmail.com>
2025-04-03 14:28:56 +0200

    1.3 1.4 - Insuring initial communication between backend and frontend

--- CarManagementApp/src/app/app.component.ts
+++ CarManagementApp/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Client, WeatherForecast } from './api/api-reference';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,10 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'CarManagementApp';
+
+  constructor(private client: Client) {
+    client.getWeatherForecast().subscribe(res => {
+      console.log(res);
+    });
+  }
 }
--- CarManagementBackend/CarManagement.Api/Program.cs
+++ CarManagementBackend/CarManagement.Api/Program.cs
@@ -6,12 +6,25 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowLocalhost",
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:4200")
+                  .AllowAnyHeader()
+                  .AllowAnyMethod()
+                  .AllowCredentials();
+        });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseCors("AllowLocalhost");
 }
 
 app.UseHttpsRedirection();
